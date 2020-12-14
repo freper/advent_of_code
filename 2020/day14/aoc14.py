b61683ff4c3f599bb492802e64c63bf5d35b77fc
Fredrik Persson <thefredrikpersson@gmail.com>
2020-12-14 09:59:25 +0100

    WIP for solution 2020 day 14

---
+++ 2020/day14/aoc14.py
@@ -0,0 +1,94 @@
+from copy import deepcopy
+import re
+
+pmask = re.compile(r"^mask = ([0,1,X]{36})$")
+pmem = re.compile(r"^mem\[(\d+)\] = (\d+)$")
+
+class Puzzle:
+  def __init__(self, filename):
+    self.read_input(filename)
+    self.memory = {}
+    
+  def read_input(self, filename):
+    file = open(filename, 'r')
+    data = file.read()
+    self.input = data.splitlines()
+
+  def update_mask(self, mask):
+    assert len(mask) == 36
+    self.bitmask = 0
+    self.mask = 0
+    self.floatbits = []
+    self.floatmask = 0
+    for offset, char in enumerate(mask):
+      if char != 'X':
+        bitmask = 2**(35 - offset)
+        value = int(char) * bitmask
+        self.bitmask = self.bitmask | bitmask
+        self.mask = self.mask | value
+      else:
+        self.floatbits.append(35 - offset)
+        self.floatmask |= 2**(35 - offset)
+
+  def update_memory(self, address, value):
+    if address not in self.memory.keys():
+      self.memory[address] = 0
+    self.memory[address] = (value & ~self.bitmask) | self.mask
+
+  def update_memory2(self, address):
+    if address not in self.memory.keys():
+      self.memory[address] = 0
+    self.memory[address] |= self.mask
+
+    if self.floatbits:
+      addresses = []
+      bit = self.floatbits[0]
+      addresses.append(((address & ~self.floatmask) | self.mask) & ~(2**bit))
+      addresses.append(((address & ~self.floatmask) | self.mask) | 2**bit)
+    for bit in self.floatbits[1:]:
+      prev_addresses = deepcopy(addresses)
+      for address in prev_addresses:
+        # print((address & ~self.floatmask) | self.mask)
+        # addresses.append(((address & ~self.floatmask) | self.mask) & ~(2**bit))
+        addresses.append(address | 2**bit)
+    addresses.sort()
+    print(addresses)
+
+    for address in addresses:
+      if address not in self.memory.keys():
+        self.memory[address] = 0
+      self.memory[address] |= self.mask
+
+  def part1(self):
+    for line in self.input:
+      if pmask.fullmatch(line):
+        m = pmask.fullmatch(line)
+        self.update_mask(m.group(1))
+      elif pmem.fullmatch(line):
+        m = pmem.fullmatch(line)
+        address = int(m.group(1))
+        value = int(m.group(2))
+        self.update_memory(address, value)
+    return sum(self.memory.values())
+
+  def part2(self):
+    for line in self.input:
+      if pmask.fullmatch(line):
+        m = pmask.fullmatch(line)
+        self.update_mask(m.group(1))
+      elif pmem.fullmatch(line):
+        m = pmem.fullmatch(line)
+        address = int(m.group(1))
+        self.update_memory2(address)
+    print(self.memory)
+    return sum(self.memory.values())
+
+test1 = Puzzle('test1.txt')
+assert test1.part1() == 165
+test2 = Puzzle('test2.txt')
+print(test2.part2())
+# assert test2.part2() == 208
+
+puzzle = Puzzle('input.txt')
+print(puzzle.part1())
+# print(puzzle.part2())
