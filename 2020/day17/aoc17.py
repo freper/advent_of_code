f0a23e7c9cbbed12a706c3a358ff055840400b9a
Fredrik Persson <thefredrikpersson@gmail.com>
2020-12-17 22:15:20 +0100

    Update solution for 2020 day 17

--- 2020/day17/aoc17.py
+++ 2020/day17/aoc17.py
@@ -10,30 +10,71 @@ class Puzzle:
         data = file.read()
         lines = data.splitlines()
 
-        num_cycles = 6
-        size1 = len(lines[0])
-        size2 = size1 + 2 * (num_cycles + 1)
-        offset1 = (size1 - 1) // 2
-        offset2 = (size2 - 1) // 2
-        grid1 = np.zeros([size2, size2, size2], dtype=int)
-        grid2 = np.zeros([size2, size2, size2, size2], dtype=int)
+        assert len(lines) == len(lines[0])
+        size = len(lines)
+        grid1 = np.zeros([size, size, 1], dtype=int)
+        grid2 = np.zeros([size, size, 1, 1], dtype=int)
         for y, line in enumerate(lines):
             for x, c in enumerate(line):
                 if c == '#':
-                    grid1[x - offset1 + offset2, y - offset1 + offset2, offset2] = 1
-                    grid2[x - offset1 + offset2, y - offset1 + offset2, offset2, offset2] = 1
+                    grid1[x, y, 0] = 1
+                    grid2[x, y, 0, 0] = 1
         self.grid1 = grid1
         self.grid2 = grid2
-        self.size = size2
 
-    def update_state1(self):
+    def prepare_grid1(self):
+        idx = np.nonzero(self.grid1)
+        xmin = np.min(idx[0])
+        xmax = np.max(idx[0])
+        ymin = np.min(idx[1])
+        ymax = np.max(idx[1])
+        zmin = np.min(idx[2])
+        zmax = np.max(idx[2])
+        dx = xmax - xmin + 1
+        dy = ymax - ymin + 1
+        dz = zmax - zmin + 1
+        grid = np.zeros([dx + 4, dy + 4, dz + 4], dtype=int)
+        grid[2: (dx + 2),
+             2: (dy + 2),
+             2: (dz + 2)] = self.grid1[xmin: (xmax + 1),
+                                       ymin: (ymax + 1),
+                                       zmin: (zmax + 1)]
+        self.grid1 = grid
+
+    def prepare_grid2(self):
+        idx = np.nonzero(self.grid2)
+        xmin = np.min(idx[0])
+        xmax = np.max(idx[0])
+        ymin = np.min(idx[1])
+        ymax = np.max(idx[1])
+        zmin = np.min(idx[2])
+        zmax = np.max(idx[2])
+        wmin = np.min(idx[3])
+        wmax = np.max(idx[3])
+        dx = xmax - xmin + 1
+        dy = ymax - ymin + 1
+        dz = zmax - zmin + 1
+        dw = wmax - wmin + 1
+        grid = np.zeros([dx + 4, dy + 4, dz + 4, dz + 4], dtype=int)
+        grid[2: (dx + 2),
+             2: (dy + 2),
+             2: (dz + 2),
+             2: (dw + 2)] = self.grid2[xmin: (xmax + 1),
+                                       ymin: (ymax + 1),
+                                       zmin: (zmax + 1),
+                                       wmin: (wmax + 1)]
+        self.grid2 = grid
+
+    def update_grid1(self):
+        self.prepare_grid1()
         updated_grid = self.grid1.copy()
-        for x in range(1, self.size - 1):
-            for y in range(1, self.size - 1):
-                for z in range(1, self.size - 1):
+        (nx, ny, nz) = updated_grid.shape
+        for x in range(1, nx - 1):
+            for y in range(1, ny - 1):
+                for z in range(1, nz - 1):
                     active = (self.grid1[x, y, z] == 1)
                     num_active_neigbours = np.sum(
-                        self.grid1[(x-1):(x+2), (y-1):(y+2), (z-1):(z+2)]) - self.grid1[x, y, z]
+                        self.grid1[(x - 1):(x + 2), (y - 1):(y + 2), (z - 1):(z + 2)]) - self.grid1[x, y, z]
                     if active and (num_active_neigbours not in [2, 3]):
                         updated_grid[x, y, z] = 0
                     elif not active and num_active_neigbours == 3:
@@ -42,12 +83,14 @@ class Puzzle:
                         updated_grid[x, y, z] = self.grid1[x, y, z]
         self.grid1 = updated_grid
 
-    def update_state2(self):
+    def update_grid2(self):
+        self.prepare_grid2()
         updated_grid = self.grid2.copy()
-        for x in range(1, self.size - 1):
-            for y in range(1, self.size - 1):
-                for z in range(1, self.size - 1):
-                    for w in range(1, self.size - 1):
+        (nx, ny, nz, nw) = updated_grid.shape
+        for x in range(1, nx - 1):
+            for y in range(1, ny - 1):
+                for z in range(1, nz - 1):
+                    for w in range(1, nw - 1):
                         active = (self.grid2[x, y, z, w] == 1)
                         num_active_neigbours = np.sum(
                             self.grid2[(x-1):(x+2), (y-1):(y+2), (z-1):(z+2), (w-1):(w+2)]) - self.grid2[x, y, z, w]
@@ -61,12 +104,12 @@ class Puzzle:
 
     def part1(self):
         for _ in range(6):
-            self.update_state1()
+            self.update_grid1()
         return np.sum(self.grid1)
 
     def part2(self):
         for _ in range(6):
-            self.update_state2()
+            self.update_grid2()
         return np.sum(self.grid2)
 
 
