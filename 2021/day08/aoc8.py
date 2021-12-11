4ecc652478a82fafad3abfd7b599b58e687b1ed9
Fredrik Persson <thefredrikpersson@gmail.com>
2021-12-11 19:40:01 +0100

    Solution for 2021 day 8 part 2

--- 2021/day08/aoc8.py
+++ 2021/day08/aoc8.py
@@ -1,3 +1,7 @@
+fieldstr_to_num = {"abcefg": 0, "cf": 1, "acdeg": 2, "acdfg": 3, "bcdf": 4,
+                   "abdfg": 5, "abdefg": 6, "acf": 7, "abcdefg": 8, "abcdfg": 9}
+
+
 class Puzzle:
     def __init__(self, filename):
         self.read_input(filename)
@@ -16,23 +20,38 @@ class Puzzle:
 
     @staticmethod
     def decode(patterns, digits):
-        decoded = dict()
-        undecoded = list()
-        table = dict()
+        decoded_sets = {}
+        patterns_by_length = {}
         for pattern in patterns:
-            if len(pattern) == 2:
-                decoded[1] = set(pattern)
-            elif len(pattern) == 3:
-                decoded[7] = set(pattern)
-            elif len(pattern) == 4:
-                decoded[4] = set(pattern)
-            elif len(pattern) == 7:
-                decoded[8] = set(pattern)
+            length = len(pattern)
+            if length in patterns_by_length.keys():
+                patterns_by_length[length].append(set(pattern))
             else:
-                undecoded.append(set(pattern))
-        table['a'] = decoded[7] - decoded[1]
-        print(table)
-        return 1
+                patterns_by_length[length] = [set(pattern)]
+        decoded_sets['a'] = patterns_by_length[3][0] - patterns_by_length[2][0]
+        decoded_sets['d'] = set.intersection(*patterns_by_length[5]) & patterns_by_length[4][0]
+        decoded_sets['f'] = set.intersection(*patterns_by_length[6]) & patterns_by_length[2][0]
+        decoded_sets['g'] = set.intersection(
+            *patterns_by_length[5]) - decoded_sets['a'] - decoded_sets['d']
+        decoded_sets['b'] = set.intersection(
+            *patterns_by_length[6]) - decoded_sets['a'] - decoded_sets['f'] - decoded_sets['g']
+        decoded_sets['c'] = patterns_by_length[3][0] - decoded_sets['a'] - decoded_sets['f']
+        decoded_sets['e'] = patterns_by_length[7][0] - set.intersection(
+            *patterns_by_length[6]) - decoded_sets['c'] - decoded_sets['d']
+
+        decoded_to_original_values = {}
+        for original_value, decoded_set in decoded_sets.items():
+            assert len(decoded_set) == 1
+            decoded_value = next(iter(decoded_set))
+            decoded_to_original_values[decoded_value] = original_value
+
+        decoded_str = ""
+        for digit in digits:
+            original_fields = [decoded_to_original_values[value] for value in digit]
+            original_str = "".join(sorted(original_fields))
+            original_num = fieldstr_to_num[original_str]
+            decoded_str += str(original_num)
+        return int(decoded_str)
 
     def part1(self):
         num_instances = 0
@@ -46,14 +65,12 @@ class Puzzle:
         sum = 0
         for patterns, digits in zip(self.patterns, self.digits):
             sum += self.decode(patterns, digits)
-        return 2
+        return sum
 
 
 test = Puzzle('test.txt')
 assert test.part1() == 26
-# assert test.part2() == 61229
-print("Part 1:", test.part1())
-print("Part 2:", test.part2())
+assert test.part2() == 61229
 
 puzzle = Puzzle('input.txt')
 print("Part 1:", puzzle.part1())
