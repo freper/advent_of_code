30d9842b6d62677f3d57bc7c24464da988ce37f3
Fredrik Persson <thefredrikpersson@gmail.com>
2021-12-15 22:59:03 +0100

    Slow solution for 2021 day 15

--- 2021/day15/aoc15.py
+++ 2021/day15/aoc15.py
@@ -6,15 +6,16 @@ class Puzzle:
         file = open(filename, 'r')
         data = file.read()
         lines = data.splitlines()
+        rows = len(lines)
+        cols = len(lines[0])
 
         map = dict()
         for row, line in enumerate(lines):
             for col, value in enumerate(line):
                 map[(row, col)] = int(value)
         self.riskmap = map
+        self.target = (rows - 1, cols - 1)
 
-        rows = len(lines)
-        cols = len(lines[0])
         fullmap = dict()
         for rowtile in range(5):
             for coltile in range(5):
@@ -25,44 +26,32 @@ class Puzzle:
                     fullrisk = ((fullrisk - 1) % 9) + 1
                     fullmap[(row, col)] = fullrisk
         self.fullriskmap = fullmap
+        self.fulltarget = (5 * rows - 1, 5 * cols - 1)
+
+    @staticmethod
+    def find_minimum_risk(start, target, riskmap):
+        risks = {start: 0}
+        updated = True
+        while updated:
+            new_risks = risks.copy()
+            updated = False
+            for position, risk in risks.items():
+                for offset in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    new_position = (position[0] + offset[0], position[1] + offset[1])
+                    if not new_position in riskmap.keys():
+                        continue
+                    new_risk = risk + riskmap[new_position]
+                    if new_position not in new_risks.keys() or new_risk < new_risks[new_position]:
+                        new_risks[new_position] = new_risk
+                        updated = True
+            risks, new_risks = new_risks, risks
+        return risks[target]
 
     def part1(self):
-        new_risks = {(0, 0): 0}
-        updated = True
-        while updated:
-            risks = dict()
-            risks, new_risks = new_risks, risks
-            updated = False
-            for position, risk in risks.items():
-                for offset in [(0, 1), (1, 0)]:
-                    new_position = (position[0] + offset[0], position[1] + offset[1])
-                    if not new_position in self.riskmap.keys():
-                        continue
-                    new_risk = risk + self.riskmap[new_position]
-                    if new_position not in new_risks.keys() or new_risk < new_risks[new_position]:
-                        new_risks[new_position] = new_risk
-                    updated = True
-        assert len(risks.values()) == 1
-        return next(iter(risks.values()))
+        return self.find_minimum_risk((0, 0), self.target, self.riskmap)
 
     def part2(self):
-        new_risks = {(0, 0): 0}
-        updated = True
-        while updated:
-            risks = dict()
-            risks, new_risks = new_risks, risks
-            updated = False
-            for position, risk in risks.items():
-                for offset in [(0, 1), (1, 0)]:
-                    new_position = (position[0] + offset[0], position[1] + offset[1])
-                    if not new_position in self.fullriskmap.keys():
-                        continue
-                    new_risk = risk + self.fullriskmap[new_position]
-                    if new_position not in new_risks.keys() or new_risk < new_risks[new_position]:
-                        new_risks[new_position] = new_risk
-                    updated = True
-        assert len(risks.values()) == 1
-        return next(iter(risks.values()))
+        return self.find_minimum_risk((0, 0), self.fulltarget, self.fullriskmap)
 
 
 test = Puzzle('test.txt')
