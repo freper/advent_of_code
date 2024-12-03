1f12f3412f49aad4d4f2040687456213cf112c74
Fredrik Persson <thefredrikpersson@gmail.com>
2024-12-03 22:56:57 +0100

    Update solution for 2024 day 2 part 2

--- 2024/day02/aoc2.py
+++ 2024/day02/aoc2.py
@@ -1,3 +1,22 @@
+def is_safe(report) -> bool:
+        if report[0] == report[1]:
+            return False
+        increase = report[1] > report[0]
+        decrease = report[1] < report[0]
+        previous = report[0]
+        for value in report[1:]:
+            if value == previous:
+                return False
+            if abs(value - previous) > 3:
+                return False
+            if increase and (value < previous):
+                return False
+            if decrease and (value > previous):
+                return False
+            previous = value
+        return True
+
+
 class Puzzle:
     def __init__(self, filename):
         self.read_input(filename)
@@ -8,57 +27,25 @@ class Puzzle:
         def parse_line(line):
             return [int(value) for value in line.split()]
         self.reports = [parse_line(line) for line in lines]
-        
-    
+   
     def part1(self):
-        def is_safe(report) -> bool:
-            if report[0] == report[1]:
-                return False
-            increase = report[1] > report[0]
-            decrease = report[1] < report[0]
-            previous = report[0]
-            for value in report[1:]:
-                if value == previous:
-                    return False
-                if abs(value - previous) > 3:
-                    return False
-                if increase and (value < previous):
-                    return False
-                if decrease and (value > previous):
-                    return False
-                previous = value
-            return True
-        
         sum = 0
         for report in self.reports:
             if is_safe(report):
                 sum += 1
         return sum
 
-
     def part2(self):
-        def diff(report):
-            return [a - b for a, b in zip(report[:-1], report[1:])]
-
-        def is_safe(report) -> bool:
-            num_faults = 0
-            
-
-            
-            
-        
         sum = 0
         for report in self.reports:
-            if is_safe(report):
+            if any([is_safe(report[:i] + report[i+1:]) for i in range(len(report))]):
                 sum += 1
         return sum
 
 
 test = Puzzle('test.txt')
-print("Part 1:", test.part1())
-print("Part 2:", test.part2())
-# assert test.part1() == 2
-# assert test.part2() == 4
+assert test.part1() == 2
+assert test.part2() == 4
 
 puzzle = Puzzle('input.txt')
 print("Part 1:", puzzle.part1())
